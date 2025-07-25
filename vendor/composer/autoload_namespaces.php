c1c2f5eeedb43592b2934c71988dd9cb2740d6c6
Berkat Saragih <148747954+btnnty12@users.noreply.github.com>
2025-07-25 14:35:00 +0700

    butuh domain inima tolong

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,5 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Detection' => array($vendorDir . '/mobiledetect/mobiledetectlib/namespaced'),
-    'Barryvdh' => array($vendorDir . '/barryvdh/reflection-docblock/src'),
 );
